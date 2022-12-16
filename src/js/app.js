495865bebb87eef50310b8e5c7691ffb0446e9e6
dzuba110729 <dzyuba1107@outlook.com>
2022-12-16 15:53:21 +0300

    new commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// import Bowerman from './Bowerman';
+// import getAattackOptions from './service';
+
+// const obj = new Bowerman('Leon');
+
+// const arrAttackOptions = getAattackOptions(obj);
+
+// console.log(arrAttackOptions);
---
+++ src/js/service.js
@@ -0,0 +1,12 @@
+export default function getAattackOptions(obj) {
+	const arr = [];
+	obj.special.forEach((element) => {
+	  const {
+		 id, name, icon, description = 'Описание недоступно',
+	  } = element;
+	  arr.push({
+		 id, name, icon, description,
+	  });
+	});
+	return arr;
+ }
